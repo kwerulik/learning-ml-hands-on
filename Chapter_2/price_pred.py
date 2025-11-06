f11ab759899df64c58ac5a505ec3f17d5aaaf457
kwerulik <kwerulik@gmail.com>
2025-11-06 16:52:23 +0100

    RandomizedSearchCV

--- Chapter_2/price_pred.py
+++ Chapter_2/price_pred.py
@@ -1,3 +1,5 @@
+from scipy.stats import randint
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics.pairwise import rbf_kernel
 from sklearn.cluster import KMeans
 from sklearn.base import BaseEstimator, TransformerMixin
@@ -300,7 +302,16 @@ parm_grid = [
     {'preprocessing__geo__n_clusters': [10, 15],
      'random_forest__max_features': [6, 8, 10]}
 ]
-grid_search = GridSearchCV(full_pipeline, parm_grid,
-                           cv=3, scoring='neg_root_mean_squared_error')
-grid_search.fit(housing, housing_labels)
-print(grid_search.best_params_)
+# grid_search = GridSearchCV(full_pipeline, parm_grid,
+#                            cv=3, scoring='neg_root_mean_squared_error')
+# grid_search.fit(housing, housing_labels)
+# print(grid_search.best_params_)
+
+
+param_distribs = {
+    'preprocessing__geo__n_clusters': randint(low=3, high=50),
+    'random_forest__max_features': randint(low=2, high=20)
+}
+rnd_search = RandomizedSearchCV(full_pipeline, param_distributions=param_distribs,
+                                n_iter=10, cv=3, scoring='neg_root_mean_squared_error', random_state=42)
+rnd_search.fit(housing, housing_labels)
